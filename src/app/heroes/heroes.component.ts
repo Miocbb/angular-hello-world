3bdaf0da89b657c1d60c434e19b40575535f6675
YuncaiMei <meiyuncai@hotmai.com>
2021-08-26 16:52:28 -0400

    src/app: add hero data fetching service and message service.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,28 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+    selector: 'app-heroes',
+    templateUrl: './heroes.component.html',
+    styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
     hero: Hero = {
         id: 1,
         name: "Windstrom"
     };
-
-    heroes = HEROES;
-
+    heroes: Hero[] = [];
     selectedHero?: Hero;
+
+    constructor(private heroService: HeroService,
+        private messageService: MessageService) { }
+
     onSelect(hero: Hero): void {
         this.selectedHero = hero;
+        this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
     }
 
-  constructor() { }
+    getHeroes(): void {
+        // using `subscribe()` to select elements asynchronously.
+        this.heroService.getHeroes()
+            .subscribe((heros_t) => this.heroes = heros_t);
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.getHeroes();
+    }
 
 }
