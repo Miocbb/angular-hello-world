1f5777006cfb5eaea400b038b03ecd251521c858
YuncaiMei <meiyuncai@hotmai.com>
2021-08-26 17:34:40 -0400

    src/app: add router for navigation.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,20 +9,10 @@ import { MessageService } from '../message.service';
     styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-    hero: Hero = {
-        id: 1,
-        name: "Windstrom"
-    };
     heroes: Hero[] = [];
-    selectedHero?: Hero;
 
     constructor(private heroService: HeroService,
         private messageService: MessageService) { }
-
-    onSelect(hero: Hero): void {
-        this.selectedHero = hero;
-        this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-    }
 
     getHeroes(): void {
         // using `subscribe()` to select elements asynchronously.
